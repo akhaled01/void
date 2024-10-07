b57065b25e4a99d4225df7ec87cafdace26351f5
akhaled01 <akhaledlarus@gmail.com>
2024-10-07 23:25:51 +0300

    reconfigured base url for vercel

--- void-docs/docusaurus.config.ts
+++ void-docs/docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
 
   // Set the production url of your site here
   url: "https://your-docusaurus-site.example.com",
-  baseUrl: "/void/",
+  baseUrl: "/",
   organizationName: "akhaled01",
   projectName: "void",
 
